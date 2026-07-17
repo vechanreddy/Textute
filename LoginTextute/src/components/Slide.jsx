0a26b5724a8125f8785cf63ccd72047620674971
fayaz <shaikfayaz2801@gmail.com>
2026-07-18 01:40:37 +0530

    modified slide

--- LoginTextute/src/components/Slide.jsx
+++ LoginTextute/src/components/Slide.jsx
@@ -12,6 +12,9 @@ function Slide() {
             <div><input type="submit" /></div>
           </form>
         </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores maxime saepe animi perspiciatis beatae illo numquam et minima deserunt nam. Obcaecati molestias voluptates cum aliquam ab incidunt placeat optio officia quibusdam ducimus aut provident numquam sunt unde minus quae, minima modi nemo pariatur sit iure rerum! Repellendus recusandae cupiditate, at voluptas vero quidem odit sapiente alias, harum quibusdam aliquam commodi optio blanditiis. Unde libero eum nostrum officia ullam, cum aperiam quisquam magni? Illo consequatur vitae, mollitia quaerat architecto atque cumque adipisci consectetur veritatis numquam dicta quo tenetur quae velit!</p>
+        <p>+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis accusamus sunt amet ipsum enim voluptatum ex unde. Sit veritatis nemo vitae error totam numquam. Cumque nemo dolor quia, iste amet, natus eligendi voluptas porro similique explicabo saepe ea numquam dolorem! Corrupti rerum repellendus quasi quidem sed exercitationem, iure consectetur recusandae! Itaque et ea nisi quisquam nostrum!</p>
     </div>
   )
 }
