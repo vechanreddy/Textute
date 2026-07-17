a515f3429ddd18c947a87d72e4014ba56aaf490a
fayaz <shaikfayaz2801@gmail.com>
2026-07-18 01:35:27 +0530

    added slide

---
+++ LoginTextute/src/components/Slide.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Slide() {
+  return (
+    <div>
+        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum commodi eos magnam unde, odit velit tempore in dolores voluptates sapiente optio id cumque qui eligendi maiores omnis quis nostrum? Excepturi odio sunt suscipit omnis fugit magnam, repudiandae possimus nemo.
+        </p>
+    </div>
+  )
+}
+
+export default Slide
