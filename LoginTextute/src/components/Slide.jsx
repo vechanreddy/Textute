9c1a8384b6cb98758652a6bee8cb804e34f14800
vechan <vechanreddygaddam@gmail.com>
2026-07-18 02:28:47 +0530

    added sign up

--- LoginTextute/src/components/Slide.jsx
+++ LoginTextute/src/components/Slide.jsx
@@ -4,6 +4,14 @@ function Slide() {
   return (
     <div>
       <h1>new slide</h1>
+
+      <div>
+        <h1>Sign up</h1>
+        <form action="">
+          <div><input type="text" /></div>
+          <div><input type="submit" /></div>
+        </form>
+      </div>
     </div>
   )
 }
