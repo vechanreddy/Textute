0cb961704831b104c560aac6dc6c28d71177c303
vechan <vechanreddygaddam@gmail.com>
2026-07-18 01:29:37 +0530

    added component Navbar

---
+++ LoginTextute/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam praesentium assumenda nobis illo, veritatis accusamus harum iure minus ullam mollitia aliquam dignissimos perferendis tenetur illum nesciunt alias voluptatibus eveniet odio.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
