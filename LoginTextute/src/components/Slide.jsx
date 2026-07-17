2d17730ad4eff6d2cfb4e51829e0d5c6c3339764
vechan <vechanreddygaddam@gmail.com>
2026-07-18 02:16:03 +0530

    modified slide from scratch

--- LoginTextute/src/components/Slide.jsx
+++ LoginTextute/src/components/Slide.jsx
@@ -3,29 +3,7 @@ import React from 'react'
 function Slide() {
   return (
     <div>
-        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum commodi eos magnam unde, odit velit tempore in dolores voluptates sapiente optio id cumque qui eligendi maiores omnis quis nostrum? Excepturi odio sunt suscipit omnis fugit magnam, repudiandae possimus nemo.
-        </p>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> eabe648f0895160a839a043f2bc8a858d438d6a7
-
-        <div>
-          <form action="">
-            <div><input type="text" /></div>
-            <div><input type="submit" /></div>
-          </form>
-        </div>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores maxime saepe animi perspiciatis beatae illo numquam et minima deserunt nam. Obcaecati molestias voluptates cum aliquam ab incidunt placeat optio officia quibusdam ducimus aut provident numquam sunt unde minus quae, minima modi nemo pariatur sit iure rerum! Repellendus recusandae cupiditate, at voluptas vero quidem odit sapiente alias, harum quibusdam aliquam commodi optio blanditiis. Unde libero eum nostrum officia ullam, cum aperiam quisquam magni? Illo consequatur vitae, mollitia quaerat architecto atque cumque adipisci consectetur veritatis numquam dicta quo tenetur quae velit!</p>
-        <p>+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis accusamus sunt amet ipsum enim voluptatum ex unde. Sit veritatis nemo vitae error totam numquam. Cumque nemo dolor quia, iste amet, natus eligendi voluptas porro similique explicabo saepe ea numquam dolorem! Corrupti rerum repellendus quasi quidem sed exercitationem, iure consectetur recusandae! Itaque et ea nisi quisquam nostrum!</p>
-<<<<<<< HEAD
-=======
->>>>>>> parent of a550b50 (added form in slide)
-=======
->>>>>>> 15cbc6f8d6476a955aa37e765869bdce8ce20f38
->>>>>>> eabe648f0895160a839a043f2bc8a858d438d6a7
+      <h1>new slide</h1>
     </div>
   )
 }
