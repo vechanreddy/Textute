5946f1e0ecb9ce178d49117c6969407b9a78d9c6
vechan <vechanreddygaddam@gmail.com>
2026-07-18 02:33:49 +0530

    added login

--- LoginTextute/src/components/Slide.jsx
+++ LoginTextute/src/components/Slide.jsx
@@ -4,6 +4,7 @@ function Slide() {
   return (
     <div>
       <h1>new slide</h1>
+<<<<<<< Updated upstream
 
       <div>
         <h1>Sign up</h1>
@@ -11,6 +12,13 @@ function Slide() {
           <div><input type="text" /></div>
           <div><input type="submit" /></div>
         </form>
+=======
+      <div>
+        <h2>Login</h2>
+        <div className=''>
+
+        </div>
+>>>>>>> Stashed changes
       </div>
     </div>
   )
